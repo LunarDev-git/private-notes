737f21c297e16995c1cb24684600dedc60fc12a8
LunarDev <dev@moonstruck.pro>
2026-01-02 11:41:19 +0100

    Placeholder Login Form

--- private-notes/pages/login.jsx
+++ private-notes/pages/login.jsx
@@ -1,7 +1,26 @@
 export default function Login() {
   return (
-    <main>
-      <h1>Login</h1>
+    <main className="page-header">
+      <header>
+        <h1 className="page-header__title">Login</h1>
+      </header>
+
+      {/* Placeholder Form */}
+      <form className="login">
+        <label>
+          Email{" "}
+          <input type="email" placeholder="Enter Email" id="login-email" />
+        </label>
+        <label>
+          Password
+          <input
+            type="password"
+            placeholder="Enter Password"
+            id="login-password"
+          ></input>
+        </label>
+        <button type="submit">Login</button>
+      </form>
     </main>
   );
 }
